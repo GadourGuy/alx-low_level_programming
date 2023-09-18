d80465bd55f7d0c17527e9efe41b60decb41c907
GadourGuy <ogadours1@gmail.com>
2023-09-18 10:50:40 -0700

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,8 +13,9 @@ void rev_string(char *s)
 	int length;
 	char temp;
 
-	length = strlen(s);
-	for (i = 0; i <= length / 2; ++i)
+	for (length = 0; s[length] != '\0'; length++)
+		;
+	for (i = 0; i < length / 2; ++i)
 	{
 		temp = s[i];
 		s[i] = s[length - 1 - i];
