0f1d699520ad8f80fbf534ae424f7648998d0811
GadourGuy <ogadours1@gmail.com>
2023-09-21 15:07:56 -0700

    last fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,13 +14,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
 	while (n1[i] != '\0')
-	{
 		i++;
-	}
 	while (n2[j] != '\0')
-	{
 		j++;
-	}
 	if (i > j)
 		l = i;
 	else
@@ -50,7 +46,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			return (0);
 		while (l-- > 0)
 			r[l + 1] = r[l];
-		r[0] = d + '0';
-	}
+		r[0] = d + '0';		}
 	return (r);
 }
