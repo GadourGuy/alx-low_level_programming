a2b00bd698b5a432c0ebb5114ec06945d9211ad4
GadourGuy <ogadours1@gmail.com>
2023-09-25 10:34:29 -0700

    quick fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,23 +10,12 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	char *b = "rkbqkbkr";
 
-	strcpy(a[8], b);
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (i == 0)
-				_putchar(b[j]);
-			else if (i == 2)
-				_putchar('p');
-			else if (i == 7)
-				_putchar('P');
-			else if (i == 9)
-				_putchar(b[j] - 32);
-			else
-				_putchar(' ');
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
