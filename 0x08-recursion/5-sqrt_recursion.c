1ad3564e11fa2152401ee810a56b9fe51a6716ee
GadourGuy <ogadours1@gmail.com>
2023-09-28 11:07:15 -0700

    last 2 mandatory tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * squireFinder - finds a square root
+ * @start: start of search
+ * @end: end of search
+ *
+ * Return: square root
+ */
+
+int squireFinder(int start, int end)
+{
+	if (start * start == end)
+		return (start);
+	else if (start * start < end)
+		return (squireFinder(start + 1, end));
+	else
+		return (-1);
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: input number
  *
@@ -9,12 +26,10 @@
 
 int _sqrt_recursion(int n)
 {
-	if (((n / 2) * (n /2)) == n)
-		return (n / 2);
-	else if (((n / 2) * (n /2)) > n)
-		return (_sqrt_recursion(n + 1));
-	else if (n <= 0)
+	if (n < 0)
 		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
 	else
-		return (_sqrt_recursion(n - 1));
+		return (squireFinder(1, n));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * checkPrime - checks for a prime number
+ * @start: search begining
+ * @end: search end
+ * 
+ * Return: return 1 if succsesfull 0 if not
+ */
+
+int checkPrime(int start, int end)
+{
+	if (end % start != 0 && end != start)
+		return (checkPrime(start + 1, end));
+	else if (end % start == 0 && end == start)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * is_prime_number - returns 1 if prime 0 if not
+ * @n: input number
+ *
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (checkPrime(2, n));
+}
