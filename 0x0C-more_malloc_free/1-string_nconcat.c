df8c70ce80df3a5ddc8a05acd79aa4528eb91922
GadourGuy <ogadours1@gmail.com>
2023-10-07 01:36:17 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	if (size2 >= check)
 		size2 = check;
-	new = malloc(size1 + n + 1);
+	new = malloc(size1 + size2);
 	if (new == NULL)
 	{
 		return (NULL);
