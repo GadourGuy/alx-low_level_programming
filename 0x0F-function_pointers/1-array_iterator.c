94db10d6b779229702a25b4bf4161c7194fae238
GadourGuy <ogadours1@gmail.com>
2023-10-11 08:17:01 -0700

    tasks until 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on an array.
+ * @array: given array.
+ * @size: size of array.
+ * @action: pointer function to be called.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: array to be searched.
+ * @size: size of array
+ * @cmp: function to be used for searching.
+ *
+ * Return: index of found number.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]))
+			return(i);
+	}
+	return (-1);
+}
