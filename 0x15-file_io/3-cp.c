028a9ccf8270c7f5eef3efef0d3fcdb270404b35
GadourGuy <ogadours1@gmail.com>
2023-10-30 14:28:02 -0700

    uploading functions for tasks until 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,10 @@
 #include "main.h"
+#define READ_BUF_SIZE 1024
 #define ARG_ERROR "Usage: cp file_from file_to\n"
 #define READ_ERROR "Error: Can't read from file %s\n"
 #define WRITE_ERROR "Error: Can't write to %s\n"
 #define CLOSE_ERROR "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - copies data from file to another
@@ -16,18 +18,18 @@ int main(int argc, char *argv[])
 {
 	int fd1 = 0, fd2 = 0;
 	ssize_t R;
-	char buffer[1024];
+	char buf[READ_BUF_SIZE];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, ARG_ERROR), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(98);
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(99);
-	while ((R = read(fd1, buffer, 1024)) > 0)
-		if (write(fd2, buffer, R) != R)
+	while ((R = read(fd1, buf, READ_BUF_SIZE)) > 0)
+		if (write(fd2, buf, R) != R)
 			dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]), exit(99);
 	if (R == -1)
 		dprintf(STDERR_FILENO, READ_ERROR, argv[1]), exit(98);
