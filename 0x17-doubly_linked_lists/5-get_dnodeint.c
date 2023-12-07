1178aeb233505e199899a4eb45b7c21c119cb2f6
GadourGuy <ogadours1@gmail.com>
2023-12-07 11:21:08 -0800

    uploding task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - searches for node
+ * @head: head of node
+ * @index: index of node
+ *
+ * Return: node or NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp = head;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (tmp);
+		i++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
