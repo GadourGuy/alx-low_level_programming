cee7d5af7fc6e2c856a654d0b6881963fc0aca88
GadourGuy <ogadours1@gmail.com>
2023-12-21 14:39:51 -0800

    uploding task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
-	long unsigned int i;
+	unsigned long int i;
 
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
-	new->array = malloc(sizeof(hash_node_t) * size);
+	new->array = malloc(sizeof(hash_table_t) * size);
 	if (new->array == NULL)
 		return (NULL);
 	for (i = 0; i < new->size; i++)
