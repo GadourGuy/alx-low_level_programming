854d007e7960b8dd3ac401e307d7f8543e15e1f1
GadourGuy <ogadours1@gmail.com>
2023-12-22 02:30:19 -0800

    uploding task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: table to be printed
+ *
+ * Return: Void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int flag;
+
+	if (!ht || !ht->array)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			if ((current->key) && (current->value))
+			{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s': '%s'", current->key, current->value);
+				flag = 1;
+			}
+			current = current->next;
+		}
+	}
+	printf("}\n");
+
+}
